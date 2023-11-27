2bbb0cd2bdc551bf36dd4dc871fdf53659b73fbb
opalissan <opal.issan@gmail.com>
2023-11-27 08:07:38 -0800

    add notebooks for SW square-root

--- SW-sqrt/bump_on_tail_instability_sw_sqrt.py
+++ SW-sqrt/bump_on_tail_instability_sw_sqrt.py
@@ -61,23 +61,23 @@ def rhs(y, t):
                                                                                         E=E,
                                                                                         u_s=u_e2)
 
-        # # momentum drift
-        # dydt_[-2] = momentum_drift_two_stream(state_e1=state_e1,
-        #                                       state_e2=state_e2,
-        #                                       state_i=state_i,
-        #                                       E=E, Nv=Nv, alpha_e1=alpha_e1,
-        #                                       alpha_e2=alpha_e2, alpha_i=alpha_i,
-        #                                       q_e1=q_e1, q_e2=q_e2, q_i=q_i, dx=dx)
-        #
-        # # energy drift
-        # dydt_[-1] = energy_drift_two_stream(state_e1=state_e1,
-        #                                     state_e2=state_e2,
-        #                                     state_i=state_i,
-        #                                     E=E, Nv=Nv,
-        #                                     alpha_e1=alpha_e1, alpha_e2=alpha_e2, alpha_i=alpha_i,
-        #                                     q_e1=q_e1, q_e2=q_e2, q_i=q_i, dx=dx,
-        #                                     m_e1=m_e1, m_e2=m_e2, m_i=m_i, Nx=Nx,
-        #                                     u_e1=u_e1, u_e2=u_e2, u_i=u_i)
+    # momentum drift
+    dydt_[-2] = momentum_drift_two_stream(state_e1=state_e1,
+                                          state_e2=state_e2,
+                                          state_i=state_i,
+                                          E=E, Nv=Nv, alpha_e1=alpha_e1,
+                                          alpha_e2=alpha_e2, alpha_i=alpha_i,
+                                          q_e1=q_e1, q_e2=q_e2, q_i=q_i, dx=dx)
+
+    # energy drift
+    dydt_[-1] = energy_drift_two_stream(state_e1=state_e1,
+                                        state_e2=state_e2,
+                                        state_i=state_i,
+                                        E=E, Nv=Nv,
+                                        alpha_e1=alpha_e1, alpha_e2=alpha_e2, alpha_i=alpha_i,
+                                        q_e1=q_e1, q_e2=q_e2, q_i=q_i, dx=dx,
+                                        m_e1=m_e1, m_e2=m_e2, m_i=m_i, Nx=Nx,
+                                        u_e1=u_e1, u_e2=u_e2, u_i=u_i)
 
     return dydt_
 
